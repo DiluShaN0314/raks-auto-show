b817a2caafff3f1dc5a34a47afe1a175f0b735d5
TBowsika <bowsikathaya@gmail.com>
2025-07-03 17:29:53 +0530

    add changes

--- sections/partners.php
+++ sections/partners.php
@@ -7,25 +7,19 @@
         <h3>Media Partner</h3>
         <img src="assets/images/partners/IMG_0610.PNG" alt="TATA" style=" width: 161px; height: 66px; padding: 10px;" id="media_partner">
         <h3>Participants</h3>
-        <div class="partner-slider">
-            <div class="partner-track">
-                <img src="assets/images/partners/449159213_10161287446263433_6557108064647473677_n.jpg" alt="MINI">
-                <img src="assets/images/partners/Background.png" alt="MMS">
-                <img src="assets/images/partners/bb9f5712-56a6-4dd5-85df-67b7f0defee9.jpg" alt="Rolls Royce">
-                <img src="assets/images/partners/hero-logo-png_seeklogo-170801.png" alt="Sixt">
-                <img src="assets/images/partners/HNB_New_Logo.png" alt="VistaJet">
-                <img src="assets/images/partners/Hyundai Logo and symbol, meaning, history, PNG, brand.jpg" alt="AlphaX">
-                <img src="assets/images/partners/unnamed.jpg" alt="Azimut">
-
-                <!-- Duplicate for seamless loop -->
-                <img src="assets/images/partners/449159213_10161287446263433_6557108064647473677_n.jpg" alt="MINI">
-                <img src="assets/images/partners/Background.png" alt="MMS">
-                <img src="assets/images/partners/bb9f5712-56a6-4dd5-85df-67b7f0defee9.jpg" alt="Rolls Royce">
-                <img src="assets/images/partners/hero-logo-png_seeklogo-170801.png" alt="Sixt">
-                <img src="assets/images/partners/HNB_New_Logo.png" alt="VistaJet">
-                <img src="assets/images/partners/Hyundai Logo and symbol, meaning, history, PNG, brand.jpg" alt="AlphaX">
-                <img src="assets/images/partners/unnamed.jpg" alt="Azimut">
+        <!-- New 3D Participant Section -->
+        <section class="participant-carousel-3d">
+            <div class="carousel-3d-container">
+                <div class="carousel-3d-track">
+                    <img src="assets/images/partners/449159213_10161287446263433_6557108064647473677_n.jpg" alt="MINI">
+                    <img src="assets/images/partners/Background.png" alt="MMS">
+                    <img src="assets/images/partners/bb9f5712-56a6-4dd5-85df-67b7f0defee9.jpg" alt="Rolls Royce">
+                    <img src="assets/images/partners/hero-logo-png_seeklogo-170801.png" alt="Hero">
+                    <img src="assets/images/partners/HNB_New_Logo.png" alt="HNB">
+                    <img src="assets/images/partners/Hyundai Logo and symbol, meaning, history, PNG, brand.jpg" alt="Hyundai">
+                    <img src="assets/images/partners/unnamed.jpg" alt="Azimut">
+                </div>
             </div>
-        </div>
+        </section>
     </div>
 </section>
